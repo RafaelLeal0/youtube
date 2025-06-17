a0bd2bce654f0e937f3567937a517601b303030a
RafaelLeal0 <rleal3105@gmail.com>
2025-06-17 15:19:14 -0300

    prontoo

--- youtube-clone/script.js
+++ youtube-clone/script.js
@@ -6,3 +6,10 @@ function toggleSidebar() {
   const sidebar = document.getElementById('sidebar');
   sidebar.classList.toggle('expanded');
 }
+  const seta = document.querySelector(".categorias .seta");
+  const scrollArea = document.querySelector(".categorias .scroll");
+
+  seta.addEventListener("click", () => {
+    scrollArea.scrollBy({ left: 150, behavior: "smooth" });
+  });
+
